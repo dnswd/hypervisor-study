4bcca4308665b81fb5dd905425ecc5ba5b593f27
Dennis Al Baihaqi Walangadi <dennis.walangadi@gmail.com>
2024-11-21 11:47:00 +0700

    chore: use tandasat gcc fork

--- flake.nix
+++ flake.nix
@@ -33,6 +33,68 @@
             rust.stable.latest.default.override {
               extensions = [ "rust-src" "rustfmt" ];
             };
+
+        bochs-tandasat = final.stdenv.mkDerivation {
+          pname = "bochs";
+          version = "tandasat-fork";
+
+          # src = ./project/Bochs/bochs; # Path to the Bochs source
+          src = final.fetchFromGitHub {
+            owner = "tandasat";
+            repo = "Bochs";
+            rev = "gcc";
+            sha256 = "sha256-n5KEC8IsXmSP/tjb+Y5KJsW6HEdCtCBnlfdf5g6ahK0=";
+          };
+
+          nativeBuildInputs = [
+            final.libtool
+            final.autoconf
+            final.automake
+            final.bison
+            final.flex
+            final.cmake
+          ];
+
+          buildInputs = [
+            final.ncurses
+            final.xorg.libX11
+            final.xorg.libXrandr
+            final.xorg.libXinerama
+            final.xorg.libXcursor
+          ];
+
+          configurePhase = # sh
+            ''
+              pushd bochs || return 1
+              sh .conf.linux
+            '';
+
+          buildPhase = ''
+            sh .conf.linux
+            make
+          '';
+
+          installPhase = # sh
+            ''
+              make install \
+              prefix=$out \
+              exec_prefix=$out \
+              bindir=$out/bin \
+              libdir=$out/lib \
+              plugdir=$out/lib/bochs/plugins \
+              mandir=$out/share/man \
+              docdir=$out/share/doc/bochs \
+              sharedir=$out/share/bochs
+            '';
+
+          postInstall = ''
+            # Set correct plugin path
+            sed -i "s|/usr/local/lib/bochs/plugins|$out/lib/bochs/plugins|g" $out/bin/bochs
+            
+            # Set correct shared resource path
+            sed -i "s|/usr/local/share/bochs|$out/share/bochs|g" $out/bin/bochs
+          '';
+        };
       };
 
       devShells = forEachSupportedSystem ({ pkgs }: {
@@ -46,9 +108,23 @@
             cargo-watch
             rust-analyzer
 
-            # Bochs for emu
-            bochs
+            # requirements
+            p7zip
+            inetutils
           ];
+
+          buildInputs = [ pkgs.bochs-tandasat ];
+
+          # shellHook = ''
+          #   export PATH=$PATH:${pkgs.bochs}/bin
+          # '';
+
+          shellHook = ''
+            # Export LTDL_LIBRARY_PATH for Bochs plugins
+            export LTDL_LIBRARY_PATH=${pkgs.bochs-tandasat}/lib/bochs/plugins
+            # Export BXSHARE for Bochs shared resources
+            export BXSHARE=${pkgs.bochs-tandasat}/share/bochs
+          '';
 
           env = {
             # Required by rust-analyzer
